ae3a32aee7b243769ffd630bc3b099d0088705b8
A Anandha Krishnan <anandha.krishnan@bigbinary.com>
2022-09-02 13:06:00 +0530

    Added tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   MAX_EMAIL_LENGTH = 255
+  MAX_NAME_LENGTH = 35
 
   before_destroy :assign_tasks_to_task_owners
 
@@ -11,7 +12,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :authentication_token
 
-  validates :name, presence: true, length: { maximum: 35 }
+  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
