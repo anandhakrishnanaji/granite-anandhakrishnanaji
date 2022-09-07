5df72bc08aa52b7248a5b5931a27ec0ca417377c
A Anandha Krishnan <anandha.krishnan@bigbinary.com>
2022-09-07 13:24:56 +0530

    Added test cases for sidekiq email sending feature

--- app/jobs/task_logger_job.rb
+++ app/jobs/task_logger_job.rb
@@ -4,15 +4,9 @@ class TaskLoggerJob < ApplicationJob
   sidekiq_options queue: :default, retry: 3
   queue_as :default
 
-  after_create :log_task_details
-
   def perform(task)
     msg = "A task was created with the following title: #{task.title}"
     log = Log.create! task_id: task.id, message: msg
     puts log.message
-  end
-
-  def log_task_details
-    TaskLoggerJob.perform_later(self)
   end
 end
